2de6b7a7bf796030271a040d1361c82fad7a787a
SinaMostafanejad <m_mostafanejad@hotmail.com>
2019-10-02 18:45:48 -0400

    adds the temporary ability to switch between USE_LIBXC ON and OFF without changing the code

--- src/libmcpdft/energy.cc
+++ src/libmcpdft/energy.cc
@@ -1,10 +1,14 @@
 #include <armadillo>
 #include <sys/sysinfo.h>
-//#include <xc.h>
 #include "energy.h"
 #include "mcpdft.h"
-#include "functional.h"
 #include "libMem.h"
+#include "openrdmConfig.h"
+#ifdef WITH_LIBXC
+   #include <xc.h>
+#else
+   #include "functional.h"
+#endif
 
 namespace mcpdft {
 
@@ -44,46 +48,54 @@ namespace mcpdft {
       size_t npts = mc->get_npts();
       arma::vec tr_rhoa(mc->get_tr_rhoa());
       arma::vec tr_rhob(mc->get_tr_rhob());
-      //arma::vec tr_rho(tr_rhoa);
-      //arma::vec ex(npts);
-      //arma::vec ec(npts);
-      //arma::vec W(mc->get_w());
-      //tr_rho = tr_rho + tr_rhob;
-      //double * rhop = tr_rho.memptr();
-      //double * exp = ex.memptr();
-      //double * ecp = ec.memptr();
+#ifdef WITH_LIBXC
+      arma::vec tr_rho(tr_rhoa);
+      arma::vec ex(npts);
+      arma::vec ec(npts);
+      arma::vec W(mc->get_w());
+      tr_rho = tr_rho + tr_rhob;
+      double * rhop = tr_rho.memptr();
+      double * exp = ex.memptr();
+      double * ecp = ec.memptr();
 
+      double Ex = 0.0;
+      double Ec = 0.0;
+
+      xc_func_type func_x;
+      if(xc_func_init(&func_x,XC_LDA_X, XC_UNPOLARIZED) != 0){
+        xc_lda_exc(&func_x, npts, rhop, exp);
+      }
+
+      double ans = 0.0;
+      for (int p = 0; p < npts; p++) {
+          if (tr_rho(p) > 1.0e-20)
+             ans += W(p) * ex(p) * tr_rho(p);
+      }
+      Ex=ans;
+
+      xc_func_type func_c;
+      if(xc_func_init(&func_c,XC_LDA_C_VWN_RPA, XC_UNPOLARIZED) != 0){
+        xc_lda_exc(&func_c, npts, rhop, ecp);
+      }
+
+      ans = 0.0;
+      for (int p = 0; p < npts; p++) {
+          if (tr_rho(p) > 1.0e-20)
+             ans += W(p) * ec(p) * tr_rho(p);
+      }
+      Ec=ans;
+      xc_func_end(&func_x);
+      xc_func_end(&func_c);
+#else
       Functional* func = new Functional;
 
       double Ex = 0.0;
       double Ec = 0.0;
       Ex = func->EX_LSDA(mc, tr_rhoa, tr_rhob);
       Ec = func->EC_VWN3(mc, tr_rhoa, tr_rhob);
-      // xc_func_type func_x;
-      // if(xc_func_init(&func_x,XC_LDA_X, XC_UNPOLARIZED) != 0){
-      //   xc_lda_exc(&func_x, npts, rhop, exp);
-      // }
 
-      // double ans = 0.0;
-      // for (int p = 0; p < npts; p++) {
-      //     if (tr_rho(p) > 1.0e-20)
-      //        ans += W(p) * ex(p) * tr_rho(p);
-      // }
-      // Ex=ans;
-
-      // xc_func_type func_c;
-      // if(xc_func_init(&func_c,XC_LDA_C_VWN_RPA, XC_UNPOLARIZED) != 0){
-      //   xc_lda_exc(&func_c, npts, rhop, ecp);
-      // }
-
-      // ans = 0.0;
-      // for (int p = 0; p < npts; p++) {
-      //     if (tr_rho(p) > 1.0e-20)
-      //        ans += W(p) * ec(p) * tr_rho(p);
-      // }
-      // Ec=ans;
-      // xc_func_end(&func_x);
-      // xc_func_end(&func_c);
+      delete func;
+#endif
 
       printf("------------------------------------------\n");
       printf("   Classical energy = %-20.12lf\n", eclass);
@@ -95,7 +107,6 @@ namespace mcpdft {
       tot_energy += Ex;
       tot_energy += Ec;
 
-      delete func;
       delete libmem;
 
       return tot_energy;
