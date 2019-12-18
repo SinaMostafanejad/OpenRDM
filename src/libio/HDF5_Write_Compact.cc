43f2c27bba2340dba68930d0cbb6ef0e489a083f
SinaMostafanejad <m_mostafanejad@hotmail.com>
2019-12-18 10:44:55 -0500

    finishes the COMPACT HDF5 layout implementation

--- src/libio/HDF5_Write_Compact.cc
+++ src/libio/HDF5_Write_Compact.cc
@@ -28,17 +28,15 @@ namespace mcpdft {
 
       /* file indentifiers and handles */
       hid_t file_id;
+      hid_t dcpl_D1a, dcpl_D1b;
       hid_t D1a_grp_id, D1b_grp_id;
       hid_t D1a_dst_id, D1b_dst_id;
       hid_t dataspace_id;
-      hid_t filespace_id;//, memspace_id;
-      hid_t prop_id;
+
       /* dataset dimensions at creation time */
       double D1_buff[dim][dim];
       hsize_t dims[2] = {dim, dim};
-      // hsize_t      maxdims[2] = {H5S_UNLIMITED, H5S_UNLIMITED};
       herr_t       status;
-      // hsize_t      chunk_dims[2] = {2, 5};
 
       /* Create a new file. If the file exists, its contents
        * will be overwritten */
@@ -55,15 +53,23 @@ namespace mcpdft {
       /* Create the data space for the datasets */
       dataspace_id = H5Screate_simple(RANK, dims, NULL);
 
+      /* Create the dataset creation property lists */
+      dcpl_D1a = H5Pcreate (H5P_DATASET_CREATE);
+      dcpl_D1b = H5Pcreate (H5P_DATASET_CREATE);
+
+      /* Set the dcpls' layout to COMPACT */
+      status = H5Pset_layout (dcpl_D1a, H5D_COMPACT);
+      status = H5Pset_layout (dcpl_D1b, H5D_COMPACT);
+
       /* Create a dataset in group "/D1a" */
       D1a_dst_id = H5Dcreate2(file_id, D1A_DATASET,
         	              H5T_IEEE_F64LE, dataspace_id,
-                              H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+                              H5P_DEFAULT, dcpl_D1a, H5P_DEFAULT);
 
       /* Create a dataset in group "/D1b" */
       D1b_dst_id = H5Dcreate2(file_id, D1B_DATASET,
         	              H5T_IEEE_F64LE, dataspace_id,
-                              H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+                              H5P_DEFAULT, dcpl_D1b, H5P_DEFAULT);
 
       for(int i=0; i < dim; i++)
          for(int j=0; j < dim; j++)
@@ -81,22 +87,22 @@ namespace mcpdft {
       status = H5Dwrite(D1b_dst_id, H5T_NATIVE_DOUBLE,
         	        H5S_ALL, H5S_ALL, H5P_DEFAULT, D1_buff);
 
-      // /* Modify dataset creation properties, i.e. enable chunking  */
-      // prop_id = H5Pcreate (H5P_DATASET_CREATE);
-      // status = H5Pset_chunk (prop_id, RANK, chunk_dims);
-
-      /* Close dataspace. */
+      /* Close dataspace */
       status = H5Sclose(dataspace_id);
 
-      /* Close datasets. */
+      /* Close property lists */
+      status = H5Pclose (dcpl_D1a);
+      status = H5Pclose (dcpl_D1b);
+
+      /* Close datasets */
       status = H5Dclose(D1a_dst_id);
       status = H5Dclose(D1b_dst_id);
 
-      /* Close groups. */
+      /* Close groups */
       status = H5Gclose(D1a_grp_id);
       status = H5Gclose(D1b_grp_id);
 
-      /* Close the file. */
+      /* Close the file */
       status = H5Fclose(file_id); 
    }
 
@@ -106,17 +112,15 @@ namespace mcpdft {
 
       /* file indentifiers and handles */
       hid_t file_id;
+      hid_t dcpl_D2ab;
       hid_t D2ab_grp_id;
       hid_t D2ab_dst_id;
       hid_t dataspace_id;
-      hid_t filespace_id;//, memspace_id;
-      hid_t prop_id;
+
       /* dataset dimensions at creation time */
       double D2_buff[dim][dim];
       hsize_t dims[2] = {dim, dim};
-      // hsize_t      maxdims[2] = {H5S_UNLIMITED, H5S_UNLIMITED};
       herr_t       status;
-      // hsize_t      chunk_dims[2] = {2, 5};
 
       /* Create a new file. If the file exists, its contents
        * will be overwritten */
@@ -130,10 +134,16 @@ namespace mcpdft {
       /* Create the data space for the dataset */
       dataspace_id = H5Screate_simple(RANK, dims, NULL);
 
+      /* Create the dataset creation property list */
+      dcpl_D2ab = H5Pcreate (H5P_DATASET_CREATE);
+
+      /* Set the dcpl layout to COMPACT */
+      status = H5Pset_layout (dcpl_D2ab, H5D_COMPACT);
+
       /* Create a dataset in group "/D2ab" */
       D2ab_dst_id = H5Dcreate2(file_id, D2AB_DATASET,
         	              H5T_IEEE_F64LE, dataspace_id,
-                              H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+                              H5P_DEFAULT, dcpl_D2ab, H5P_DEFAULT);
 
       for(int ij=0; ij < dim; ij++)
          for(int kl=0; kl < dim; kl++)
@@ -143,20 +153,19 @@ namespace mcpdft {
       status = H5Dwrite(D2ab_dst_id, H5T_NATIVE_DOUBLE,
         	        H5S_ALL, H5S_ALL, H5P_DEFAULT, D2_buff);
 
-      // /* Modify dataset creation properties, i.e. enable chunking  */
-      // prop_id = H5Pcreate (H5P_DATASET_CREATE);
-      // status = H5Pset_chunk (prop_id, RANK, chunk_dims);
-
-      /* Close dataspace. */
+      /* Close dataspace */
       status = H5Sclose(dataspace_id);
 
-      /* Close dataset. */
+      /* Close property lists */
+      status = H5Pclose (dcpl_D2ab);
+
+      /* Close dataset */
       status = H5Dclose(D2ab_dst_id);
 
-      /* Close group. */
+      /* Close group */
       status = H5Gclose(D2ab_grp_id);
 
-      /* Close the file. */
+      /* Close the file */
       status = H5Fclose(file_id); 
    }
 }
