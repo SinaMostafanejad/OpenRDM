7792493177a5141b17ad10c3740824e207b99ecb
SinaMostafanejad <m_mostafanejad@hotmail.com>
2020-01-16 08:13:50 -0500

    getting the core energy

--- tests/h2_pyscf.py
+++ tests/h2_pyscf.py
@@ -20,10 +20,13 @@ myhf.kernel()
 nao = mol.nao_nr()
 
 cmat_mo = myhf.mo_coeff
+print("AO2MO transformation matrix C =")
 print(cmat_mo)
 cmat_mo_occ = myhf.mo_occ
 
-overlap = myhf.get_ovlp()
+smat = myhf.get_ovlp()
+print("overlap matrix S =")
+print(smat)
 
 #dmat = myhf.make_rdm1()
 #dtemp = np.matmul(cmat_mo.transpose(),dmat)
@@ -32,8 +35,16 @@ dmat_mo = np.zeros((nao,nao))
 dmat_mo[0][0] = 1.0
 print(dmat_mo)
 
-eigval, eigvec = myhf.eig(dmat_mo,overlap)
-print(eigval)
+#eigval, eigvec = myhf.eig(dmat_mo,smat)
+#print(eigval)
+
+hcore_ao = myhf.get_hcore()
+htemp = np.matmul(cmat_mo.transpose(),hcore_ao)
+hcore_mo = np.matmul(htemp,cmat_mo)
+print(hcore_mo)
+
+core_en = 2.0 * np.vdot(dmat_mo,hcore_mo)
+print("core energy = %15.10lf\n" % core_en)
 
 jmat_ao = myhf.get_j()
 jtemp = np.matmul(cmat_mo.transpose(),jmat_ao)
@@ -41,7 +52,7 @@ jmat_mo = np.matmul(jtemp,cmat_mo)
 jmat_mo *= 0.5
 print(jmat_mo)
 
-ca = 2.0 * np.vdot(dmat_mo,jmat_mo)
-print("Coulomb energy = %15.10lf\n" % ca)
+coulomb_en = 2.0 * np.vdot(dmat_mo,jmat_mo)
+print("Classical Coulomb energy = %15.10lf\n" % coulomb_en)
 
 
